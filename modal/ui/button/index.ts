4fe3c85a6aa97bc0f73ca037ab51d5e061e41216
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2021-11-17 12:10:57 -0300

    Added exports for Primary and Secondary Buttons

--- modal/ui/button/index.ts
+++ modal/ui/button/index.ts
@@ -1,2 +1,4 @@
-export { Button } from './button';
-export type { ButtonProps } from './button';
+import { PrimaryButton, SecondaryButton } from "./button.composition";
+export { Button } from "./button";
+export type { ButtonProps } from "./button";
+export { PrimaryButton, SecondaryButton } from "./button.composition";
