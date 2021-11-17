c60c3c3b9c00212abd0d8f859b2abfd2d4549aa5
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2021-11-17 11:30:41 -0300

    Added missing import on button.spec.tsx

--- modal/ui/button/button.spec.tsx
+++ modal/ui/button/button.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/dom";
 import { PrimaryButton } from "./button.composition";
 import { Button, ButtonProps } from "./index";
 
